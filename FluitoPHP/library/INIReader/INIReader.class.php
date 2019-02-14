1a88c0cef4f5474ce9019e18594693b2a0da88e4
Vipin Jain <vipin.jn.24@gmail.com>
2019-02-14 19:08:41 +0530

    Added support for environment variables in INI configuration file

--- FluitoPHP/library/INIReader/INIReader.class.php
+++ FluitoPHP/library/INIReader/INIReader.class.php
@@ -24,7 +24,8 @@ namespace FluitoPHP\INIReader;
  * Variables:
  *      1. $inheritance
  *      2. $parseSections
- *      3. $INIArray
+ *      3. $decodeEnvars
+ *      4. $INIArray
  *
  * Functions:
  *      1. __construct
@@ -56,6 +57,15 @@ class INIReader extends \FluitoPHP\File\File {
     private $parseSections = true;
 
     /**
+     * Used for storing decodeEnvars setting.
+     *
+     * @var bool
+     * @author Neha Jain
+     * @since  0.1
+     */
+    private $decodeEnvars = true;
+
+    /**
      * Used for storing INI generated array.
      *
      * @var array
@@ -72,6 +82,9 @@ class INIReader extends \FluitoPHP\File\File {
      *                    'parseSections' => Provide true if sections are needed to be parsed.
      *                    'inheritance' => Provide true for inheritance of other file on the same path.
      *                                     Requires 'extends=<file to be inherited>' inside the INI.
+     *                    'decodeEnvars' => Provide true to update the %ENVAR% to be updated with the
+     *                                      environment variables from OS/Web Server OR set variable
+     *                                      set explicitly.
      * @author Neha Jain
      * @since  0.1
      */
@@ -88,6 +101,8 @@ class INIReader extends \FluitoPHP\File\File {
                 inheritance = isset($args['inheritance']) ? (bool) $args['inheritance'] : true;
         $this->
                 parseSections = isset($args['parseSections']) ? (bool) $args['parseSections'] : true;
+        $this->
+                decodeEnvars = isset($args['decodeEnvars']) ? (bool) $args['decodeEnvars'] : true;
     }
 
     /**
@@ -125,12 +140,44 @@ class INIReader extends \FluitoPHP\File\File {
      */
     public function Fetch() {
 
-        if (!$this->
-                INIArray) {
+        if ($this->
+                INIArray === NULL) {
+
+            $INIString = $this->
+                    Read();
+
+            if ($this->
+                    decodeEnvars) {
+
+                $offset = 0;
+
+                while (($fi = strpos($INIString, '%', $offset)) !== false) {
+
+                    if (substr($INIString, $fi + 1, 1) === '%') {
+
+                        $offset = $fi + 2;
+                        continue;
+                    }
+
+                    $fie = strpos($INIString, '%', $fi + 1);
+
+                    if ($fie !== false) {
+
+                        $envarName = substr($INIString, $fi + 1, $fie - $fi - 1);
+                        $envarValue = getenv($envarName);
+
+                        if ($envarValue !== false) {
+
+                            $INIString = substr($INIString, 0, $fi) . $envarValue . substr($INIString, $fie + 1);
+                        }
+                    }
+                }
+
+                $INIString = str_replace('%%', '%', $INIString);
+            }
 
             $this->
-                    INIArray = parse_ini_file($this->
-                            GetPath(), $this->
+                    INIArray = parse_ini_string($INIString, $this->
                     parseSections);
 
             if ($this->
