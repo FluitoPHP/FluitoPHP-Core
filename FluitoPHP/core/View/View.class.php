ec8b73d8dd4846181b0241d88be20c41225effca
Vipin Jain <vipin.jn.24@gmail.com>
2019-07-02 17:31:31 +0530

    Updated View class to resolve default controller and view issues

--- FluitoPHP/core/View/View.class.php
+++ FluitoPHP/core/View/View.class.php
@@ -145,6 +145,10 @@ class View extends \FluitoPHP\Base\Base {
      */
     public function Run() {
 
+
+        $this->
+                RefreshView();
+
         if (!file_exists($this->
                         view) &&
                 !file_exists(MODULE . DS . $this->
